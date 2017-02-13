5cd0d2282760267ee25f98750960ad3a9544dc9d
tomer <marx.tomer@gmail.com>
2017-02-13 15:52:43 +0200

    finished

--- SQL/adventure.sql
+++ SQL/adventure.sql
@@ -10,22 +10,23 @@ CREATE TABLE `users`(
 );
 
 CREATE TABLE `images` (
+  `image_id` int(11) auto_increment,
   `image_name` varchar(500) default NULL,
   `story_id` int(11),
   `adventure_id` int(11),
-  PRIMARY KEY  (`story_id`)
+  PRIMARY KEY  (`image_id`)
   );
 
 INSERT INTO `images` VALUES
-('inbed.jpg', 1, 1),
-('clock.jpg', 2, 1),
-('stairs.jpg', 3, 1),
-('coffee_and_computer.jpeg', 4, 1),
-('phone_interview.jpg', 5, 2),
-('interview.jpg', 6, 2),
-('salary.jpg', 7, 2),
-('work.jpeg', 8, 2),
-('victory.jpg', 0, 0);
+(default, 'inbed.jpg', 1, 1),
+(default, 'clock.jpg', 2, 1),
+(default, 'stairs.jpg', 3, 1),
+(default, 'coffee_and_computer.jpeg', 4, 1),
+(default, 'phone_interview.jpg', 1, 2),
+(default, 'interview.jpg', 2, 2),
+(default, 'salary.jpg', 3, 2),
+(default, 'work.jpeg', 4, 2),
+(default, 'victory.jpg', 0, 0);
 
 CREATE TABLE `story` (
   `id` int(11),
@@ -35,8 +36,7 @@ CREATE TABLE `story` (
   `content` varchar(1000) default NULL,
   `life_unit` int(11) default NULL,
   `wealth_unit` int(11) default NULL,
-  PRIMARY KEY  (`id`),
-  FOREIGN KEY (story_id) REFERENCES images(story_id)
+  PRIMARY KEY  (`id`)
   );
 
 INSERT INTO `story` VALUES
@@ -60,26 +60,26 @@ INSERT INTO `story` VALUES
 (18, 1, 4, 2, 'You throw the coffee cup and your ruined computer in the trash', 60, 10),
 (19, 1, 4, 3, 'You tell Shy its the other persons fault and that they should pay for the computer damage', 60, 5),
 (20, 1, 4, 4, 'You realize that maybe you shouldnt have been drinking coffee in the classroom', 10, 4),
-(21, 2, 5, 0, 'You finally got your first interview and you get a question you have no idea how to do', 0, 0),
-(22, 2, 5, 1, 'You totally break down and tell them you cant do it', 100, 10),
-(23, 2, 5, 2, 'You struggle a lot but ask them questions and sort of get the problem right', 50, 4),
-(24, 2, 5, 3, 'You struggle a lot but dont talk and dont ask them anything', 80, 6),
-(25, 2, 5, 4, 'You tell them why the question they asked is stupid and then you answer your own question', 100, 10),
-(26, 2, 6, 0, 'You make it to the second round of interviews', 0, 0),
-(27, 2, 6, 1, 'Youre so excited that you spend all night partying and getting wasted', 100, 10),
-(28, 2, 6, 2, 'You get to the interview late but then do a pretty good job', 50, 5),
-(29, 2, 6, 3, 'You get to the interview early and are not sure how you did, but you did everything ITC trained you to do', 30, 2),
-(30, 2, 6, 4, 'You freak out mid interview', 100, 8),
-(31, 2, 7, 0, 'You get an offer and need to negotiate salary', 0, 0),
-(32, 2, 7, 1, 'You give them a number way below your worth', 50, 10),
-(33, 2, 7, 2, 'You tell them to pay you whatever they think is best', 30, 5),
-(34, 2, 7, 3, 'You engage in a back and forth with them about a salary you feel is fair', 10, 1),
-(35, 2, 7, 4, 'You hire someone to negotiate for you', 100, 10),
-(36, 2, 8, 0, 'You got the job! You have your first day of work', 0, 0),
-(37, 2, 8, 1, 'You pick an appropriate outfit and get there early', 0, 1),
-(38, 2, 8, 2, 'You dont leave much extra time to get there and you show up a little late', 20, 4),
-(39, 2, 8, 3, 'You ask if you can work from home because youre erally nervous', 50, 8),
-(40, 2, 8, 4, 'You take everyone in your new office out to breakfast', 30, 10)
+(21, 2, 1, 0, 'You finally got your first interview and you get a question you have no idea how to do', 0, 0),
+(22, 2, 1, 1, 'You totally break down and tell them you cant do it', 100, 10),
+(23, 2, 1, 2, 'You struggle a lot but ask them questions and sort of get the problem right', 50, 4),
+(24, 2, 1, 3, 'You struggle a lot but dont talk and dont ask them anything', 80, 6),
+(25, 2, 1, 4, 'You tell them why the question they asked is stupid and then you answer your own question', 100, 10),
+(26, 2, 2, 0, 'You make it to the second round of interviews', 0, 0),
+(27, 2, 2, 1, 'Youre so excited that you spend all night partying and getting wasted', 100, 10),
+(28, 2, 2, 2, 'You get to the interview late but then do a pretty good job', 50, 5),
+(29, 2, 2, 3, 'You get to the interview early and are not sure how you did, but you did everything ITC trained you to do', 30, 2),
+(30, 2, 2, 4, 'You freak out mid interview', 100, 8),
+(31, 2, 3, 0, 'You get an offer and need to negotiate salary', 0, 0),
+(32, 2, 3, 1, 'You give them a number way below your worth', 50, 10),
+(33, 2, 3, 2, 'You tell them to pay you whatever they think is best', 30, 5),
+(34, 2, 3, 3, 'You engage in a back and forth with them about a salary you feel is fair', 10, 1),
+(35, 2, 3, 4, 'You hire someone to negotiate for you', 100, 10),
+(36, 2, 4, 0, 'You got the job! You have your first day of work', 0, 0),
+(37, 2, 4, 1, 'You pick an appropriate outfit and get there early', 0, 1),
+(38, 2, 4, 2, 'You dont leave much extra time to get there and you show up a little late', 20, 4),
+(39, 2, 4, 3, 'You ask if you can work from home because youre really nervous', 50, 8),
+(40, 2, 4, 4, 'You take everyone in your new office out to breakfast', 30, 10)
 ;
 
 CREATE TABLE `adventure` (
